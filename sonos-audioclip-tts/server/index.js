01aad212941cbf3faf26fec7d89891ade0a45f5d
Cameron Schaer <cameronschaer@gmail.com>
2022-08-11 00:31:34 -0400

    manually edit urls

--- sonos-audioclip-tts/server/index.js
+++ sonos-audioclip-tts/server/index.js
@@ -62,7 +62,7 @@ const oauth2 = simpleOauthModule.create({
 
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: 'https://hassio.local:8349/redirect',
+  redirect_uri: '192.168.1.68:8349/redirect',
   scope: 'playback-control-all',
   state: 'none',
 });
@@ -102,7 +102,7 @@ app.get('/auth', async (req, res) => {
 // redirect service parsing the authorization token and asking for the access token
 app.get('/redirect', async (req, res) => {
   const code = req.query.code;
-  const redirect_uri = 'https://hassio.local:8349/redirect';
+  const redirect_uri = '192.168.1.68:8349/redirect';
 
   const options = {
     code,redirect_uri,
